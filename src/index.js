ac224af5708ded408f3ef25ca89e572629c3daca
ryowuandjanet <ryowudata@gmail.com>
2020-12-30 11:09:28 +0800

    react-setup-tutorial初始化

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import reactDOM from 'react-dom';
+import './styles/style.scss';
+
+const App = () => {
+  return <h1>Hello World</h1>
+}
+
+reactDOM.render(<App />, document.getElementById('root'));
---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.join(__dirname, 'public'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [{
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader']
+      }
+    ]
+  },
+  devtool: 'chep-module-eval-source-map',
+  devServer: {
+    contentBase: path.join(__dirname, 'public')
+  }
+}
